13594ad14f54f625c576145c37719ca24cf25755
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-14 01:21:15 +0600

    added login routes and removed unnecessary files

---
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div className="d-flex flex-column justify-content-center align-items-center ">
+      <h1 className="display-1 fw-bold ">404</h1>
+      <h3 className="display-4">Not Found!!!</h3>
+    </div>
+  );
+};
+
+export default NotFound;
