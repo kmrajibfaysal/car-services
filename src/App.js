8019b013fddfa8cf6470345fc6ae2899969b817e
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-14 00:25:12 +0600

    Revert "added dynamic route of single service to see its details"
    This reverts commit d8c3a3799924c202fe51114a81d0b37b1afdd29f.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
-import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -12,10 +11,6 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
-        <Route
-          path="/service/:serviceId"
-          element={<ServiceDetail></ServiceDetail>}
-        ></Route>
         <Route path="/about" element={<About />}></Route>
       </Routes>
       <Footer></Footer>
