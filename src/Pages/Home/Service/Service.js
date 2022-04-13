6508461bd48bb22dab45904c627fa8155dab77a6
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-14 00:43:47 +0600

    added dynamic route of single service to see its details

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Service.css';
 
-const Service = ({service}) => {
-    const {name, img, description, price} = service;
-    return (
-        <div className='service'>
-            <img className='w-100' src={img} alt="" />
-            <h2>{name}</h2>
-            <p>Price: {price}</p>
-            <p><small>{description}</small></p>
-            <button className='btn btn-primary'>Book: {name}</button>
-        </div>
-    );
+const Service = ({ service }) => {
+  const { id, name, img, description, price } = service;
+
+  const navigate = useNavigate();
+
+  const handleServiceDetail = (id) => navigate(`/service/${id}`);
+
+  return (
+    <div className="service">
+      <img className="w-100" src={img} alt="" />
+      <h2>{name}</h2>
+      <p>Price: {price}</p>
+      <p>
+        <small>{description}</small>
+      </p>
+      <button
+        onClick={() => handleServiceDetail(id)}
+        className="btn btn-primary"
+      >
+        Book: {name}
+      </button>
+    </div>
+  );
 };
 
 export default Service;
