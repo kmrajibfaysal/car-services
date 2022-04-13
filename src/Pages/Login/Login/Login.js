41214c0c32a2c01b0a73fadc48709639521e3b9c
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2022-04-14 01:58:35 +0600

    added login page using react bootstrap

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+  const emailRef = useRef('');
+  const passwordRef = useRef('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const email = emailRef.current.value;
+    console.log(email);
+    const password = passwordRef.current.value;
+    console.log(password);
+  };
+
+  const navigateRegister = () => {
+    navigate('/register');
+  };
+
   return (
-    <div>
-      <h2>Please Login</h2>
+    <div className="container w-50 mx-auto mt-4" style={{ maxWidth: '600px' }}>
+      <h2 className="text-primary text-center display-5 fw-normal">
+        Please Login
+      </h2>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            ref={emailRef}
+            type="email"
+            placeholder="Enter email"
+            required
+          />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            ref={passwordRef}
+            type="password"
+            placeholder="Password"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="I agree to terms & conditions" />
+        </Form.Group>
+        <Button
+          onClick={handleSubmit}
+          className=""
+          variant="primary"
+          type="submit"
+        >
+          Submit
+        </Button>
+      </Form>
+      <p className="mt-3 text-center">
+        New to Genius Car?
+        <span
+          className="text-danger ms-2"
+          style={{ cursor: 'pointer' }}
+          onClick={navigateRegister}
+        >
+          Please Register!
+        </span>
+      </p>
     </div>
   );
 };
